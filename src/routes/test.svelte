341e884d3f461ab610917527f378eae110323943
fabezio <fabezio@outlook.fr>
2020-07-23 20:04:02 +0200

    enrichissement de la base de données (tests) ;creation classes adéquates

--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -19,13 +19,25 @@
   {#each products.categories as cat}
   <li>{cat.name}
   <ul class='second-degree'>{#each cat.content as content}
+  <div class="image">
+    <img src={content.image} alt={content.name}>
+  </div>
+
   <li>{content.name}</li>
   <li>{content.description}</li>
   <li>
   <ul class='third-degree'>{#each content.rates as rate}
+  <div>
+    <img class='avatar' src='{rate.avatar}' alt='{rate.author}'>
+  </div>
+
   <li>{rate.author}</li>
   <li>{rate.comment}</li>
-  <li>{rate.rating}</li>
+  <li>
+  <div class="progress" style="background-color: hsl({120 * (rate.rating)/5}, 100%, 50%)">
+  <!-- {rate.rating} -->
+  </div>
+  </li>
   {/each}
   </ul></li>
   {/each}
@@ -34,7 +46,7 @@
   </ul>
 </div>
 
-<style>
+<style global>
 li {
   list-style: none;
 }
@@ -50,4 +62,16 @@ ul {
 .second-degree {
   font-weight: 500;
 }
+.avatar {
+  max-width: 64px;
+  max-height: 64px;
+  border-radius: 50%;
+}
+.progress {
+  
+  width: 10rem;
+  height: 10px;
+  border-radius: 3px;
+}
+.image, img {max-width: 400px; max-height: 400px;}
 </style>
