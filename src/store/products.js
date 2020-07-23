dd06a64b24ba2c640d4dd7888ed68bdf5b49c0d7
fabezio <fabezio@outlook.fr>
2020-07-23 16:28:27 +0200

    ajout magasin produits

---
+++ src/store/products.js
@@ -0,0 +1,30 @@
+const products = [{
+    categories: [{
+        "robes": [{
+            name: 'petite robe en dentelle',
+            slug: 'petite-robe-en-dentelle',
+            description: 'idem',
+            price: 125.99,
+            rates: [{
+                    author: 'jamie',
+                    comment: 'too belle!',
+                    rating: 4
+                }, {
+                    author: 'Marcelle',
+                    comment: 'bien montée, très joli... bravo à la couturière et la modeuse!',
+                    rating: 5
+                },
+                {
+                    author: 'misha',
+                    comment: 'un peu cher...',
+                    rating: 3
+
+                }
+            ]
+
+        }]
+
+
+    }]
+}]
+export default products
